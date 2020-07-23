b2186a17554ac4d483c8c0c4b5a91df8ddfa5702
pazrvien <yjaewoong@naver.com>
2020-07-23 22:22:25 +0900

    Initial Commit

---
+++ MyNetflix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyNetflix
+//
+//  Created by 윤재웅 on 2020/07/23.
+//  Copyright © 2020 pazravien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
